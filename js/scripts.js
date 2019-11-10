ca239f0b6411a562546598b9e96705a29ba25218
apple <apple@Celestes-IMac.local>
2019-11-10 14:21:19 -0800

    add JS, CSS, README, HTML, and img folders

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var MULTIPLY = function(number1, number2){
+    return number1 * number2;
+    
+    
+  };
+  
+  var liter = parseInt (prompt("Type in 3.785411784:"));
+  var gallons = parseInt (prompt("Enter gallons amount:"));
+  var conversion = (liter * gallons);
+  var result =""
+  alert("Your gallons to liters " + liter + "3.785411784" + "\nYour gallons is " + gallons + "gallons");
+  
